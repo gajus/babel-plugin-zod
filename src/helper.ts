42b19479e3184ef7d80c04984f40e7f11633898c
Gajus <gajus@gajus.com>
2025-03-24 17:41:31 -0600

    fix: handle browser environment

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,19 @@
+/* eslint-disable no-negated-condition */
+
 import { type z } from 'zod';
 
 export const defineBuildZodSchema = (
   build: (hash: string, buildZodSchema: () => z.ZodTypeAny) => void,
 ) => {
-  // eslint-disable-next-line canonical/id-match
-  global._buildZodSchema = build;
+  if (typeof global !== 'undefined') {
+    // eslint-disable-next-line canonical/id-match
+    global._buildZodSchema = build;
+    // @ts-expect-error - window is not defined in Node.js
+  } else if (typeof window !== 'undefined') {
+    // @ts-expect-error - window is not defined in Node.js
+    // eslint-disable-next-line canonical/id-match
+    window._buildZodSchema = build;
+  } else {
+    throw new TypeError('Could not define _buildZodSchema');
+  }
 };
